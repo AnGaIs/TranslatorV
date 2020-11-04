5c650619cebad0e2e91a3ca3d6cd7d7e7af79950
AnGais <gabrieldealmeidapires@gmail.com>
2020-11-04 00:07:41 -0300

    Tradutor em Vue

---
+++ src/plugins/i18n.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+
+Vue.use(VueI18n);
+
+const messages = {
+    'en': {
+        welcomeMsg: 'Welcome to Your Vue.js App',
+        guide: 'For a guide and recipes on how to configure / customize this project,',
+        checkout: 'check out the',
+        plugins: 'Installed CLI Plugins',
+        links: 'Essential Links',
+        ecosystem: 'Ecosystem'
+    },
+    'es': {
+        welcomeMsg: 'Bienvenido a tu aplicación Vue.js',
+        guide: 'Para una guía y recetas sobre cómo configurar / personalizar este proyecto,',
+        checkout: 'revisar la',
+        plugins: 'Plugins de CLI instalados',
+        links: 'Enlaces esenciales',
+        ecosystem: 'Ecosistema'
+    },
+    'pt': {
+        welcomeMsg: 'Bem vindo a sua aplicação vue.js',
+        guide: 'Para um guia e receitas sobre como configurar / personalizar este projeto,',
+        checkout: 'Confira',
+        plugins: 'Plugins Instalados por CLI',
+        links: 'Links Essenciais',
+        ecosystem: 'Ecossistema'
+    }
+};
+
+const i18n = new VueI18n({
+    locale: 'en', // set locale
+    fallbackLocale: ['es', 'pt'], // set fallback locale
+    messages, // set locale messages
+});
+
+export default i18n;
